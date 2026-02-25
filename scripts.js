d6e03b6101e99b57d0156d759584439be052dfdf
velibor918 <velibor918@gmail.com>
2026-02-25 18:36:52 +0100

    Added checkWIn functions to createUser object

--- scripts.js
+++ scripts.js
@@ -13,15 +13,21 @@ function createUser (name, marker) {
     function makePlay (num, marker) {
         return function (secondNum) {
             if(matrix[num][secondNum] === '') {
-            matrix[num][secondNum] = marker; }
+                matrix[num][secondNum] = marker; }
             else alert("You cant do that!");
         }
     };
 
+    function checkWin () {
+        if ( (matrix[0][0] === marker && matrix[1][1] === marker && matrix[2][2] === marker) || (matrix[0][2] === marker && matrix[1][1] === marker && matrix[2][0] === marker) || (matrix[0][0] === marker && matrix[1][0] === marker && matrix[2][0] === marker) || (matrix[0][1] === marker && matrix[1][1] === marker && matrix[2][1] === marker) || (matrix[0][2] === marker && matrix[1][2] === marker && matrix[2][2] === marker) || (matrix[0][0] === marker && matrix[0][1] === marker && matrix[0][2] === marker) || (matrix[1][0] === marker && matrix[1][1] === marker && matrix[1][2] === marker) || (matrix[2][0] === marker && matrix[2][1] === marker && matrix[2][2] === marker)) {
+            alert(true)
+        }
+    }
+
     const play0 = makePlay(0, marker);
     const play1 = makePlay(1, marker);
     const play2 = makePlay(2, marker);
-    return {name, marker, play0, play1, play2};
+    return {name, marker, play0, play1, play2, checkWin};
 };
 
 const playerOne = createUser('One', 'X');
@@ -30,7 +36,10 @@ const playerTwo = createUser('Two', 'O');
 console.log(playerOne);
 console.log(playerTwo);
 
-playerOne.play0(2);
-playerTwo.play0(2);
+playerOne.play2(0);
+playerOne.play2(1);
+playerOne.play2(2);
+playerOne.checkWin();
+
 
 console.table(matrix);
