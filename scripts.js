4fcb621d52c44f4049e40b174b36945d55a28fef
velibor918 <velibor918@gmail.com>
2026-02-25 16:08:41 +0100

    Added play func to player object, lets them place marker in matrix

--- scripts.js
+++ scripts.js
@@ -8,12 +8,28 @@ const matrix = ( () => {
     return arr; }
 )();
 
-console.table(matrix);
-
-matrix[0][2] = "xD";
-
-console.table(matrix);
-
 function createUser (name, marker) {
-    return {name, marker}
+
+    function makePlay (num, marker) {
+        return function (secondNum) {
+            alert(matrix[num][secondNum] === '');
+            matrix[num][secondNum] = marker;
+        }
+    };
+
+    const play0 = makePlay(0, marker);
+    const play1 = makePlay(1, marker);
+    const play2 = makePlay(2, marker);
+    return {name, marker, play0, play1, play2};
 };
+
+const playerOne = createUser('One', 'X');
+const playerTwo = createUser('Two', 'O');
+
+console.log(playerOne);
+console.log(playerTwo);
+
+playerOne.play0(2);
+playerOne.play0(2);
+
+console.table(matrix);
